53a2f97a72772b9ca4d9469fc5319620d9689901
MC-PM-2023 <mc-pm@datasolve-analytics.com>
2025-09-05 14:31:02 +0530

    updated vercel deployment link

--- Crudfrontend/src/services/api.js
+++ Crudfrontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_URL = "http://34.133.75.1:5000/api/users";
-
+// const API_URL = "http://34.133.75.1:5000/api/users";
+const API_URL = "https://cruddb-wei7.vercel.app/api/users";
 export const getUsers = () => axios.get(API_URL);
 export const addUser = (user) => axios.post(API_URL, user);
 export const updateUser = (id, user) => axios.put(`${API_URL}/${id}`, user);
