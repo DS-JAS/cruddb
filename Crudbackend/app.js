e46fbed66662dae87f230b8bc3fda7859b190aa9
MC-PM-2023 <mc-pm@datasolve-analytics.com>
2025-09-05 11:34:58 +0530

    updatd app.js

--- Crudbackend/app.js
+++ Crudbackend/app.js
@@ -1,3 +1,23 @@
+// import express from 'express';
+// import cors from 'cors';
+// import userRoutes from './routes/userRoutes.js';
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/users', userRoutes);
+
+
+// const PORT = 8080;
+// app.listen(PORT, () => {
+//   console.log("✅ Server running on port 5000");
+// });
+
+
+// export default app;
+
+
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
@@ -8,11 +28,11 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+// Cloud Run will inject PORT
+const PORT = process.env.PORT || 8080;
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log("✅ Server running on port 5000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
-
 
 export default app;
